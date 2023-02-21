fffc50156fb8362b205b7ed287e8d41676342c90
qinshijie <qinshijie@qq.com>
2023-02-21 09:19:33 +0800

    fix: 修改登录

--- router_handler/login.js
+++ router_handler/login.js
@@ -3,6 +3,24 @@ const UserModel = require('../db/user');
 const bcryptjs = require('bcryptjs');
 
 exports.login = async (req, res) => {
+    //获取定位
+    // const geoip = require('geoip-lite');
+    // var ip =
+    //     req.headers['x-forwarded-for'] ||
+    //     req.ip ||
+    //     req.connection.remoteAddress ||
+    //     req.socket.remoteAddress ||
+    //     req.connection.socket.remoteAddress ||
+    //     '';
+    // if (ip.split(',').length > 0) {
+    //     ip = ip.split(',')[0];
+    // }
+    // ip = ip.substr(ip.lastIndexOf(':') + 1, ip.length);
+    // // const ip = '116.30.228.32';
+
+    // var geo = geoip.lookup(ip);
+    // console.log(ip, geo);
+
     const { email, password } = req.body;
 
     const result = await UserModel.findOne({ email }).select('+password');
